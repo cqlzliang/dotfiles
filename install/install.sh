605d26dbf2ecbd15864b64d5fdf4a13ca12ead45
lizongliang <cqlzliang@qq.com>
2022-12-04 16:43:20 +0800

    ADD add dotbot tool

--- install/install.sh
+++
@@ -1,4 +0,0 @@
-sh ./install/starship-install.sh
-sh ./install/homebrew-install.sh
-sh ./install/fzf-install.sh
-sh ./install/zsh-install.sh
---
+++ script/install.sh
@@ -0,0 +1,5 @@
+scripHome=$HOME/dotfiles/script
+sh $scripHome/starship-install.sh
+sh $scripHome/homebrew-install.sh
+sh $scripHome/fzf-install.sh
+sh $scripHome/zsh-install.sh
