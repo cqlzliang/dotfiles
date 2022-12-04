0fc6a64ae172849780a0596eba1f978abd9a41d0
李宗梁 <lizongliang@lizongliangdeMacBook-Pro.local>
2022-12-04 20:18:56 +0800

    MOD mod local config location

--- script/localFile.sh
+++ script/localFile.sh
@@ -1,11 +1,11 @@
 # git local
-localGitConfig=$HOME/dotfiles/local_gitconfig
+localGitConfig=~/.config.local/local_gitconfig
 if [[ ! -f $localGitConfig ]]; then 
     touch $localGitConfig
 fi
 
 # local sh
-localShConfig=$HOME/dotfiles/localEnv.sh
+localShConfig=~/.config.local/localEnv.sh
 if [[ ! -f $localShConfig ]]; then 
     touch $localShConfig
 fi
