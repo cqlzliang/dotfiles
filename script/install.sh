df871f089f6efa632df5e1597f7f89792043d404
lizongliang <cqlzliang@qq.com>
2023-04-22 11:37:56 +0800

    MOD tmux 配置

--- script/install.sh
+++ script/install.sh
@@ -4,3 +4,19 @@ sh $scripHome/starship-install.sh
 sh $scripHome/homebrew-install.sh
 sh $scripHome/fzf-install.sh
 sh $scripHome/zsh-install.sh
+
+# 如果 tmux 未安装则安装
+if $(/usr/bin/which -s tmux) ; then 
+    echo "tmux have been installed"
+else 
+    echo "install tmux"
+    brew install tmux
+fi
+
+# 如果 ~/.tmux 文件夹存在则打印提示信息，否则提示未安装
+if [ -d ~/.tmux ]; then
+    echo "tmux plugins have been installed"
+else
+    echo "install tmux plugins"
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
